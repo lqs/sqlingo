9eaf7f71f4c714d82bbacf3fef64e59a35e162b3
lqs <lqs@lqs.me>
2020-06-13 23:12:39 +0800

    more and more tests

---
+++ common_test.go
@@ -0,0 +1,68 @@
+package sqlingo
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestCommon(t *testing.T) {
+	db := newMockDatabase()
+
+	dummyExp1 := expression{sql: "<dummy 1>"}
+	dummyExp2 := expression{sql: "<dummy 2>"}
+	errExp := expression{builder: func(scope scope) (string, error) {
+		return "", errors.New("error")
+	}}
+	assertValue(t, &assignment{
+		field: dummyExp1,
+		value: dummyExp2,
+	}, "<dummy 1> = <dummy 2>")
+	assertError(t, &assignment{
+		field: errExp,
+		value: dummyExp2,
+	})
+	assertError(t, &assignment{
+		field: dummyExp1,
+		value: errExp,
+	})
+	assertError(t, &assignment{
+		field: errExp,
+		value: errExp,
+	})
+	assertError(t, command("COMMAND", errExp))
+
+	sql, err := commaExpressions(scope{}, []Expression{dummyExp1, dummyExp2, dummyExp1})
+	if err != nil {
+		t.Error(err)
+	}
+	if sql != "<dummy 1>, <dummy 2>, <dummy 1>" {
+		t.Error()
+	}
+	sql, err = commaAssignments(scope{}, []assignment{
+		{field: dummyExp1, value: dummyExp1},
+		{field: dummyExp1, value: dummyExp2},
+		{field: dummyExp2, value: dummyExp2},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if sql != "<dummy 1> = <dummy 1>, <dummy 1> = <dummy 2>, <dummy 2> = <dummy 2>" {
+		t.Error()
+	}
+	_, err = commaAssignments(scope{}, []assignment{
+		{field: dummyExp1, value: dummyExp1},
+		{field: dummyExp1, value: errExp},
+	})
+	if err == nil {
+		t.Error("should get error")
+	}
+
+	db.EnableCallerInfo(true)
+	if _, err := db.Select(1).FetchFirst(); err != nil {
+		t.Error(err)
+	}
+	if !strings.HasPrefix(sharedMockConn.lastSql, "/* ") {
+		t.Error()
+	}
+}
--- update_test.go
+++ update_test.go
@@ -3,11 +3,16 @@ package sqlingo
 import "testing"
 
 func TestUpdate(t *testing.T) {
-	db := database{}
+	db := newMockDatabase()
 
-	assertValue(t, db.Update(Table1).Set(field1, field2),
-		"UPDATE `table1` SET `field1` = `field2`")
+	_, _ = db.Update(Table1).Set(field1, field2).Where(trueExpression()).Execute()
+	assertLastSql(t, "UPDATE `table1` SET `field1` = `field2` WHERE 1")
 
-	assertValue(t, db.Update(Table1).Set(field1, 10).Where(field2.Equals(2)).OrderBy(field1.Desc()).Limit(2),
-		"UPDATE `table1` SET `field1` = 10 WHERE `field2` = 2 ORDER BY `field1` DESC LIMIT 2")
+	_, _ = db.Update(Table1).
+		Set(field1, 10).
+		Where(field2.Equals(2)).
+		OrderBy(field1.Desc()).
+		Limit(2).
+		Execute()
+	assertLastSql(t, "UPDATE `table1` SET `field1` = 10 WHERE `field2` = 2 ORDER BY `field1` DESC LIMIT 2")
 }
