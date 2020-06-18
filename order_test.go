5e531e47dad96f12eb915e20560d0dea3c07968b
lqs <lqs@lqs.me>
2020-06-18 12:29:34 +0800

    fix gofmt

--- order_test.go
+++ order_test.go
@@ -8,7 +8,7 @@ import (
 func TestOrder(t *testing.T) {
 	e := expression{sql: "x"}
 	assertValue(t, orderBy{by: e}, "x")
-	assertValue(t, orderBy{by: e, desc:true}, "x DESC")
+	assertValue(t, orderBy{by: e, desc: true}, "x DESC")
 	assertError(t, orderBy{by: expression{builder: func(scope scope) (string, error) {
 		return "", errors.New("error")
 	}}})
